ea670df7b3c38706933e12c082595f9d915e739a
nathan m weller <nkas17@gmail.com>
2017-10-10 22:33:14 -0400

    updated test folder name and added  to skeleton blog page

--- src/pages/BlogPage.jsx
+++ src/pages/BlogPage.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const BlogPage = () => (
-	<p>my blog page</p>
+	<main className="main flex-wrapper">
+		<div className="flex-item main__flex-item">
+			<header className="main__header">
+				<h2 className="header header_size_large">blog<span className="dilbert">&nbsp;</span></h2>
+			</header>
+			<article className="article flex-wrapper">
+				<div className="article__content flex-item article__flex-item">
+					<div className="blog-entry">
+						a blog
+					</div>
+				</div>
+			</article>
+		</div>
+	</main>
 );
 
 export default BlogPage;
